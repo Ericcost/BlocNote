160812c8eb5a2d2f30e2b46aa7be5d07f6235079
Ericcost <eric.costerousse@laposte.net>
2023-01-31 18:06:18 +0100

    Content modification edit function

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-function Content( {activeNote} ) {
-    console.log(activeNote.title)
+function Content( {activeNote, onEditNote} ) {
+    
+    const onEditFiled = (key, value) => {
+        onEditNote({
+            ...activeNote,
+            [key]: value,
+            lastModified: Date.now(),
+        })
+    };
+
+    if(!activeNote) return <div className="no-active-note">No note selected</div>
+
     return (
         <div className='app-main'>
             <div className='app-main-note-edit'>
-                <input type='text' id='title' autoFocus />
-                <textarea id='body' placeholder='Write your note here...' />
+                <input type='text' id='title' value={activeNote.title} onChange={(e) => onEditFiled("title", e.target.value)} autoFocus />
+                <textarea id='body' value={activeNote.body} onChange={(e) => onEditFiled("body", e.target.value)} placeholder='Write your note here...' />
             </div>
             <div className='app-main-note-preview'>
                 <h1 className='preview-title'>{activeNote.title}</h1>
