5469a88d976f09f58a3196ae000d1b0e24e32906
Ericcost <eric.costerousse@laposte.net>
2023-01-31 18:30:54 +0100

    sidebar modification

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 function SideBar({ notes, onAddNote, onRemoveNote, activeNote, setActiveNote }) {
 
+    const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
     return (
         <div className='app-sidebar'>
             <div className='app-sidebar-header'>
@@ -9,7 +11,7 @@ function SideBar({ notes, onAddNote, onRemoveNote, activeNote, setActiveNote }) 
                 <button onClick={onAddNote}>Add</button>
             </div>
             <div className='app-sidebar-notes'>
-                {notes.map((note) => (
+                {sortedNotes.map((note) => (
                     <div className={`app-sidebar-note ${note.id === activeNote && "active"}`} onClick={() => setActiveNote(note.id)}>
                         <div className='sidebar-note-title'>
                             <strong>{note.title}</strong>
